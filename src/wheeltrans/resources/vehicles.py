ba4e05fa54e4f1fc99d33b2d9a2c07786bf89f75
KC-Zhang <kc.zhang.ut@gmail.com>
2024-03-07 18:31:49 +0800

    working optimization

---
+++ src/wheeltrans/resources/vehicles.py
@@ -0,0 +1,33 @@
+
+vehicles = [
+  {
+    "loadLimits": {
+      "weight": {
+        "maxLoad": 5
+      }
+    },
+    "endLocation": {
+      "latitude": 34.0623483,
+      "longitude": -118.0859541
+    },
+    "startLocation": {
+      "latitude": 34.0623483,
+      "longitude": -118.0859541
+    }
+  },
+  {
+    "loadLimits": {
+      "weight": {
+        "maxLoad": 6
+      }
+    },
+    "endLocation": {
+      "latitude": 34.0623483,
+      "longitude": -118.0859541
+    },
+    "startLocation": {
+      "latitude": 34.0623483,
+      "longitude": -118.0859541
+    }
+  }
+]
